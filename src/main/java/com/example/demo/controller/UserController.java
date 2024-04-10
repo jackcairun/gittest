1a8ade8ca678ef63bacb82efc81041562f946e7a
jackrun <awfrun@outlook.com>
2024-04-10 16:56:30 +0800

    first

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+
+@RestController
+public class UserController {
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@GetMapping("/user/{id}")
+	public User getUser(@PathVariable("id") Integer id) {
+		Optional<User> user = userRepository.findById(id);
+		return user.orElse(null);
+	}
+	
+	@GetMapping("/user")
+	public User inserUser(User user) {
+		User save = userRepository.save(user);
+		return save;
+	}
+
+}
